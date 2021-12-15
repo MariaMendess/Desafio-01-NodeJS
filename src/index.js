6dae974921a573dc11dea97bef75527b7e79b871
MariaMendess <mariaodetetenorio@gmail.com>
2021-12-14 22:19:59 -0300

    coding: Codificaçao de regras de negocio

--- src/index.js
+++ src/index.js
@@ -1,40 +1,85 @@
-const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksExistsUserAccount(req, res, next) {
+
+  const { username } = req.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return res.status(400).json({ error: "User cannot find!" })
+  }
+
+  req.user = user
+  return next()
+
 }
 
-app.post('/users', (request, response) => {
+app.post('/users', (req, res) => {
+  const { name, username } = req.body;
+  const userAlreadyExistis = users.some((user) => user.username === username)
+
+  if (userAlreadyExistis) {
+    return res.status(400).json({ error: "User Already Exists" })
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  })
+
+  const result = users.find(user => user.username === username)
+
+  return res.status(201).json(result)
+});
+
+app.use(checksExistsUserAccount)
+
+app.get('/todos', (req, res) => {
+  const { user } = req;
+  return res.status(200).json(user.todos)
+});
+
+app.post('/todos', (req, res) => {
+  const { user } = req;
+  const { title, deadline } = req.body;
+
+  user.todos.push({
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    createdAt: new Date()
+  });
+
+  return res.status(201).json(user.todos)
+});
+
+app.put('/todos/:id', (req, res) => {
+  const { title, deadline } = req.body;
+  const { user } = req;
+  const { id } = req.params;
+
+  const getTodo = user.todos.find(todo => todo.id === id)
+
+  // TODO: Alterar as infos 
+
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete('/todos/:id', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
